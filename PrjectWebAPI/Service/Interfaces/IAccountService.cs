df3efec336cc616283971e0152c025fec92a73b0
Phan Van Tinh <21424057@student.hcmus.edu.vn>
2022-11-24 18:49:07 +0700

    24/11/2022 - Fix IAccountService

--- PrjectWebAPI/Service/Interfaces/IAccountService.cs
+++ PrjectWebAPI/Service/Interfaces/IAccountService.cs
@@ -9,11 +9,7 @@ namespace Service.Interfaces
 {
     public interface IAccountService
     {
-<<<<<<< HEAD
-        bool Login(AccountInput accountInput);
-=======
         LoginOutput Login(AccountInput accountInput);
->>>>>>> ce5eef002baec05eb5b0e8632203b91ff43e4956
 
         bool ChangePassword(ChangePasswordInput changePasswordInput);
 
