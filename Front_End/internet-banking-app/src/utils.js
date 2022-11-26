277b91a15c8edb48ab834ba38ebfe32a7116ccb0
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-11-26 20:01:17 +0700

    26/11/2022 - Init Utils Font End

---
+++ Front_End/internet-banking-app/src/utils.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "http://localhost:3030/api/",
+  timeout: 5000,
+  // headers: { 'X-Custom-Header': 'foobar' }
+});
+
+export function parseJwt(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+}
