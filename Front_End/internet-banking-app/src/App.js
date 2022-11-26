b35c3f6481c1a85faf5ec8134a9e9d9e33e96bb2
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-11-26 22:12:04 +0700

    26/11/2022 - Init Routers

--- Front_End/internet-banking-app/src/App.js
+++ Front_End/internet-banking-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Component } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Component />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Component />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </Router>
   );
+}
+function RequireAuth({ children }) {
+  // if (!localStorage.App_AccessToken) {
+  //   return <Navigate to="/login" />;
+  // }
+
+  return children;
 }
 
 export default App;
---
+++ Front_End/internet-banking-app/src/components/component.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function component(props) {
+  return <div></div>;
+}
+
+export default component;
