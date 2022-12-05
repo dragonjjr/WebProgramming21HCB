66e8eca0b607cd5f725ee53ac7c4c398e52e474e
Phan Van Tinh <21424057@student.hcmus.edu.vn>
2022-12-05 22:06:21 +0700

    05/12/2022 - RSA

--- PrjectWebAPI/Common/Helpers.cs
+++ PrjectWebAPI/Common/Helpers.cs
@@ -19,45 +19,52 @@ namespace Common
         public static int JWT_Time = 0;
         public static string JWT_Issuer = string.Empty;
         public static string JWT_Audience = string.Empty;
-        public static string Private_key = "MIICWgIBAAKBgHaeweEe2m6BBw+KdODv1cJzS8rwQePtVIDFM/K4apLedcHyPJUW\r\nJmE7E0ioqxA7h4hJNG5maqV97l17CRAfRph8smJwFg5i0dYsJhjaNSCQE5An1KGO\r\n1AtzcQvZPn2b4n03shgD9TvVrX2aGgmCIwlW8fi9gKh3KSsYKmZi4Y7dAgMBAAEC\r\ngYA06/mF7ZT4jjpPNa+Vl4sf+P6MqQpMnVsBJHpbxOlPY07YW7GptjsjUA73cMD5\r\nOgXqyPZKdwkHkpqhPD474ihFTkM6YWS7TB1TQgjpQF9m5e+k5azadJEyZABbfaVZ\r\nzdEhRYA4HbLeHVXA2vL/Z+O1iDzu7gC7hWnbZWxOf8jaBQJBALizVyyWT651u2xu\r\naShYCYpWfB6QTTLnW15NwhHwPFLGPTgQ//EJpYBBOKiHlNw8G3bYpC1NrAbN3LwW\r\nnAmefssCQQCkaSd33T8zV9QdnjbZkFQmXYXhWCaSwQ0W+ot87gCthKhgEOd7Y+Nz\r\nIZnxpvUwAB1ouS0+QpMuCivTaFwbt4v3AkAq8pC3rm/yyi99pCLRnb8CKuALn1RE\r\nHOXzBLO2xhzQxoXfrpxE6RBRxViuX3Bu0Y81UGTEoAX7Qw0rszovRmqHAkAuSj0G\r\njpCA0DW0sRsYXn6S3roXHE6f+yLIWXp0jj46nKMbiSbjotgjTk6drzhRb3bYSWrn\r\noK73w31bZIKqex85AkAK2rg5DQOPId1ArSXPZwCOtVoNbS+RRLsM1Q86se0x0uIa\r\nfZj99ikYWRVK5/qMjSgcZZ/TYL7+24NSYnfNQAbq";
-        public static string Public_key = "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHaeweEe2m6BBw+KdODv1cJzS8rw\r\nQePtVIDFM/K4apLedcHyPJUWJmE7E0ioqxA7h4hJNG5maqV97l17CRAfRph8smJw\r\nFg5i0dYsJhjaNSCQE5An1KGO1AtzcQvZPn2b4n03shgD9TvVrX2aGgmCIwlW8fi9\r\ngKh3KSsYKmZi4Y7dAgMBAAE=";
-        public static string Decrypt(string data)
+        public static string PublicKey = "<RSAKeyValue><Modulus>wDMByIE1irne3d2NBJ9IWL/3iltV7kwBEBYy7zkftv/bnx13cEuzLS7l1+6Y2WcxU4KOwW+Kjxe0rMAjKI72ZK0igV3mdnY3xfDtTXNr+3+k7bzC8KnGLXN6QYNM60RcUpLHqs5pi7x7uMWLtBKfpXYCbsAAj+NhFapJkpFFdb8=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        public static string PrivateKey = "<RSAKeyValue><Modulus>wDMByIE1irne3d2NBJ9IWL/3iltV7kwBEBYy7zkftv/bnx13cEuzLS7l1+6Y2WcxU4KOwW+Kjxe0rMAjKI72ZK0igV3mdnY3xfDtTXNr+3+k7bzC8KnGLXN6QYNM60RcUpLHqs5pi7x7uMWLtBKfpXYCbsAAj+NhFapJkpFFdb8=</Modulus><Exponent>AQAB</Exponent><P>36fdifJI9dU83A3Q4i6ctX0osoUwNebGRF0nc0rtZHivATyXWPysrZQ2GmBKEFRZfjnRDLEH4awY3cZfS/aCWQ==</P><Q>2/6RCLU+8b5+4iy6gSxBRADEdgDCTTK69q6pbsiTrnT0it0x4MUkHCuFdYo9z6dLHDsosmsXCpDQMpqSmXxF1w==</Q><DP>JRF/aFOdwBDdi2NG0ZYEJxhdXGkyulxLVB1UYolymwpdhwjx1K/cNtCvvuNiox43zvHqMf5NXhvV6zvro31x0Q==</DP><DQ>Kl6W9ERkAQ8dRNY0fVhWoZA8RjXTNicFFymAfFOpDbp8tpnvV0jgsYQ4SfD8AphHwQIrzmENqP1G+9gFUAY9NQ==</DQ><InverseQ>q+nuWefoKuEHuSHR1Aaos/vbdKDE4MBocMCXURMlBG+dYVhWO2YPNidavNMcwXp0/cZOiHfj6DBAxuNQu1Jqtw==</InverseQ><D>CYB8DhWVOA6IXh+d4SSexwR2kHiDfwxy4QC38+u3Da0Iho1GYl7btNgktNAu7lCTt7U0qYuCJiDd5cx58H9g3vNhVuyFZ/XVVku7WqzAq9z+AKFTgp0W3EzN6POPixh2iGCkKArZX/ywHoyQM0ctpH02ORqaMJuHRk01mvvjpoE=</D></RSAKeyValue>";
+        public static string Encryption(string strText)
         {
-            UnicodeEncoding ByteConverter = new UnicodeEncoding();
-            var rsa = new RSACryptoServiceProvider();
-            var dataArray = data.Split(new char[] { ',' });
-            byte[] dataByte = new byte[dataArray.Length];
-            for (int i = 0; i < dataArray.Length; i++)
+            var testData = Encoding.UTF8.GetBytes(strText);
+
+            using (var rsa = new RSACryptoServiceProvider(1024))
             {
-                dataByte[i] = Convert.ToByte(dataArray[i]);
+                try
+                {
+                    rsa.FromXmlString(PublicKey.ToString());
+
+                    var encryptedData = rsa.Encrypt(testData, true);
+
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
             }
-
-            rsa.FromXmlString(Private_key);
-            var decryptedByte = rsa.Decrypt(dataByte, false);
-
-            return ByteConverter.GetString(decryptedByte);
         }
 
-        public static string Encrypt(string data)
+        private static string Decryption(string strText)
         {
-            var rsa = new RSACryptoServiceProvider();
-            rsa.FromXmlString(Public_key);
-            UnicodeEncoding ByteConverter = new UnicodeEncoding();
-            var dataToEncrypt = ByteConverter.GetBytes(data);
-            var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
-            var length = encryptedByteArray.Count();
-            var item = 0;
-            var sb = new StringBuilder();
-            foreach (var x in encryptedByteArray)
+            var testData = Encoding.UTF8.GetBytes(strText);
+
+            using (var rsa = new RSACryptoServiceProvider(1024))
             {
-                item++;
-                sb.Append(x);
+                try
+                {
+                    var base64Encrypted = strText;
+                    rsa.FromXmlString(PrivateKey);
 
-                if (item < length)
-                    sb.Append(",");
+                    var resultBytes = Convert.FromBase64String(base64Encrypted);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
             }
-
-            return sb.ToString();
         }
         public enum RoleID
         {
